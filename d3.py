e6208286bff1d77bdd4abeb25a65285f179bd3c2
Brian G <sorghumking@yahoo.com>
2021-12-03 14:39:42 -0600

    Day 3 cleaner

--- d3.py
+++ d3.py
@@ -15,25 +15,24 @@ def p1(numbers):
         epsilon.append('0' if ones > zeroes else '1')
     g_str = ''.join(gamma)
     e_str = ''.join(epsilon)
-    print(f"gamma = {g_str}, epsilon = {e_str}")
-    print(f"Product is {int(g_str,2) * int(e_str,2)}")
+    print(f"Part 1:\ngamma = {g_str}, epsilon = {e_str}")
+    print(f"Product is {int(g_str,2) * int(e_str,2)}\n")
 
 def p2(numbers):
-    oxy = get_rating(numbers, '1', '0', 'oxygen')
-    co2 = get_rating(numbers, '0', '1', 'co2')
+    print("Part 2:")
+    oxy = get_rating(numbers, 'oxygen', '1', lambda x,y: x if len(x) > len(y) else y)
+    co2 = get_rating(numbers, 'co2', '0', lambda x,y: x if len(x) < len(y) else y)
     print(f"Product of {oxy} and {co2} = {oxy*co2}")
 
-def get_rating(_numbers, keep_bit, dump_bit, name):
+def get_rating(_numbers, name, default, test):
     numbers = _numbers.copy()
-    new_numbers = []
     for idx in range(len(numbers[0])):
-        ones = len([n[idx] for n in numbers if n[idx] == '1'])
-        zeroes = len(numbers) - ones
-        if ones >= zeroes:
-            new_numbers = [n for n in numbers if n[idx] == keep_bit]
-        else: # zeroes > ones
-            new_numbers = [n for n in numbers if n[idx] == dump_bit]
-        numbers = new_numbers
+        zeroes = [n for n in numbers if n[idx] == '0']
+        ones = [n for n in numbers if n[idx] == '1']
+        if len(zeroes) == len(ones):
+            numbers = zeroes if default == '0' else ones
+        else:
+            numbers = test(zeroes, ones)
         if len(numbers) == 1:
             decimal = int(''.join(numbers[0]), 2)
             print(f"{name} rating = {numbers[0]} = {decimal}")
