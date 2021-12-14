780e27b5dbc2a9f58b0d24bc09dde1d1d69a1c93
Brian G <sorghumking@yahoo.com>
2021-12-13 18:01:11 -0600

    Day 13 cleanup

--- d13.py
+++ d13.py
@@ -19,8 +19,6 @@ def parse_input(inputfile):
     return dots, folds
 
 def do_folds(_dots, folds):
-    wid = 0
-    hit = 0
     dots = _dots.copy()
     for axis, val in folds:
         new_dots = []
@@ -30,25 +28,28 @@ def do_folds(_dots, folds):
                 diff = abs(d[1] - val) * 2
                 folded_dot = (d[0], d[1] - diff)
                 new_dots.append(folded_dot)
-            hit = val
-        else: # y-axis
+        else: # x-axis
             unmoved_dots = [dot for dot in dots if dot[0] < val]
             for d in [dot for dot in dots if dot[0] > val]:
                 diff = abs(d[0] - val) * 2
                 folded_dot = (d[0] - diff, d[1])
                 new_dots.append(folded_dot)
-            wid = val
         dots = new_dots + unmoved_dots
-        dots = list(set(dots)) # cull dups
-    return dots, wid, hit
+        dots = list(set(dots)) # cull duplicates
+    return dots
 
-def print_paper(dots, wid, hit):
-    for y in range(0, hit):
-        for x in range(0, wid):
+def print_paper(dots, width, height):
+    for y in range(0, height):
+        for x in range(0, width):
             print('#', end='') if (x,y) in dots else print(' ', end='')
         print('\n')
 
 if __name__ == "__main__":
     dots, folds = parse_input("inputs/d13.txt")
-    folded_dots, wid, hit = do_folds(dots, folds)
-    print_paper(folded_dots, wid, hit)
+    folded_dots = do_folds(dots, folds[:1])
+    print(f"Part 1: {len(folded_dots)} dots after 1 fold.")
+    folded_dots = do_folds(dots, folds)
+    width = min([f[1] for f in folds if f[0] == 'x'])
+    height = min([f[1] for f in folds if f[0] == 'y'])
+    print (f"\nPart 2:")
+    print_paper(folded_dots, width, height)
