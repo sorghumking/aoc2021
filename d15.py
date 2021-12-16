649c7f7031ef89448473d00ee4e2dd4e5e94576f
Brian G <sorghumking@yahoo.com>
2021-12-15 20:22:20 -0600

    Day 15 heapq learnings, me feel dumb

--- d15.py
+++ d15.py
@@ -1,3 +1,6 @@
+import time
+import heapq
+
 def parse_input(inputfile):
     grid = []
     with open(inputfile) as f:
@@ -5,8 +8,8 @@ def parse_input(inputfile):
             grid.append([int(c) for c in l.strip()])
     return grid
 
-# I am too dumb to derive Dijkstra's Algorithm myself.
-# God bless Reddit and Wikipedia.
+# I am too dumb to derive Dijkstra's Algorithm myself, but here is my
+# incredibly slow implementation. God bless Reddit and Wikipedia.
 def dijkstra(grid, start_pos, end_pos):
     verts = set()
     dists = {}
@@ -21,7 +24,9 @@ def dijkstra(grid, start_pos, end_pos):
     
     grid_wid = len(grid[0])
     while len(verts) > 0:
-        v = min(verts, key=lambda v:dists[v])
+        if len(verts) % 1000 == 0:
+            print(f"{len(verts)}, ", end='')
+        v = min(verts, key=lambda v:dists[v]) # this is the bottleneck
         verts.remove(v)
         if v == end_pos:
             break
@@ -33,17 +38,61 @@ def dijkstra(grid, start_pos, end_pos):
                     prevs[pt] = v
     return dists, prevs
 
+# My impl is way too slow for Part 2.
+# A heapq-based impl of Dijkstra's cribbed from StackOverflow,
+# with a tweak to not add the first point's cost. 
+# Strangely, this impl doesn't return the correct path (appears to
+# include every single vertex), but the risk is correct.
+def dijkstra_heapq(grid, start_pos, end_pos):
+    verts = [(0, start_pos, [])]
+    seen = set()
+    grid_wid = len(grid[0])
+    while True:
+        risk, v, path = heapq.heappop(verts)
+        if v not in seen:
+            path += [v]
+            seen.add(v)
+            if v == end_pos:
+                risk += grid[v[1]][v[0]]
+                return risk, path
+            for pt in get_adjacent(v[0], v[1], grid_wid):
+                new_risk = grid[v[1]][v[0]] if v != start_pos else 0 # don't add risk of start_pos
+                heapq.heappush(verts, (risk + new_risk, pt, path))
+
+
 def get_adjacent(x, y, grid_wid):
     deltas = [(0,1),(0,-1),(1,0),(-1,0)]
     candidates = [(x+dx,y+dy) for dx,dy in deltas] # orthogonally adjacent points
     return [pt for pt in candidates if pt[0] >= 0 and pt[0] < grid_wid and pt[1] >= 0 and pt[1] < grid_wid]
 
-def p1_dijkstra(grid):
-    end_point = (99,99)
-    dists, prevs = dijkstra(grid, (0,0), end_point)
-    print(f"Distance = {dists[end_point]}")
+def p1_dijkstra(grid, end_point):
+    risk, path = dijkstra_heapq(grid, (0,0), end_point)
+    print(f"Part 1: Risk = {risk}")
+
+def p2_dijkstra(megagrid, end_point):
+    risk, path = dijkstra_heapq(megagrid, (0,0), end_point)
+    print(f"Part 2: Risk = {risk}")
+
+def make_megagrid(grid):
+    megagrid = []
+    for yoff in range(5):
+        for row in grid:
+            megarow = []
+            for xoff in range(5):
+                megarow += [increase_risk(risk, xoff+yoff) for risk in row]
+            megagrid.append(megarow)
+    return megagrid
+
+def increase_risk(risk, amt):
+    return risk+amt if risk+amt <= 9 else ((risk+amt) % 9)
 
 
 if __name__ == "__main__":
     grid = parse_input("inputs/d15.txt")
-    p1_dijkstra(grid)
+    p1_dijkstra(grid, (99,99))
+    # start_time = time.time()
+    megagrid = make_megagrid(grid)
+    # print(f"Post-megagrid: {time.time() - start_time} seconds.")
+    # p2_dijkstra(megagrid, (49,49))
+    p2_dijkstra(megagrid, (499,499))
+    # print(f"Took {time.time() - start_time} seconds.")
