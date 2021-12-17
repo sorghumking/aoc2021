20a1415ba0b7b4bce3e5466ec59d4e1a4e032309
Brian G <sorghumking@yahoo.com>
2021-12-17 13:03:25 -0600

    Day 16 logging

--- d16.py
+++ d16.py
@@ -12,26 +12,30 @@ def hex_to_bin(hexstr):
     return binstr
 
 vers_sum = 0
-def parse(blob):
+def parse(blob, level):
     vers = blob.version()
     global vers_sum
     vers_sum += vers
     ti = blob.type_id()
+    print(f"{'  '*level} version {vers} type {ti}", end='')
     if ti == 4:
         litval = blob.literal_value()
+        print(f" literal value = {litval}")
     else:
         lti = blob.len_type_id()
         if lti == 0:
             sl = blob.sub_length()
+            print(f" LTI 0, reading {sl} bits")
             total_read = 0
             while total_read < sl:
                 old_pos = blob.getpos()
-                parse(blob)
+                parse(blob, level+1)
                 total_read += blob.getpos() - old_pos
         elif lti == 1:
             sc = blob.sub_count()
+            print(f" LTI 1, reading {sc} subpackets")
             for sub in range(sc):
-                parse(blob)
+                parse(blob, level+1)
 
 class Blob:
     def __init__(self, binary_blob):
@@ -93,7 +97,7 @@ class Blob:
             groups.append(next)
             if next[0] != '1':
                 break
-        print(f"Read {len(groups)} groups: {groups}")
+        # print(f"Read {len(groups)} groups: {groups}")
         value = ""
         for g in groups:
             value += g[1:]
@@ -101,8 +105,8 @@ class Blob:
     
 
 if __name__ == "__main__":
-    hex = parse_input("inputs/d16.txt")
-    # for hex in ["A0016C880162017C3686B18A3D4780"]:
-    parse(Blob(hex_to_bin(hex)))
-    print(f"Sum = {vers_sum}")
-    vers_sum = 0
+    # hex = parse_input("inputs/d16.txt")
+    for hex in ["C200B40A82"]:
+        parse(Blob(hex_to_bin(hex)), 0)
+        print(f"Sum = {vers_sum}")
+        vers_sum = 0
