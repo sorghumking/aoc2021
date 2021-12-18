b81ee3a69b7e1c8712c7e672a4d85e4fe8fccfca
Brian G <sorghumking@yahoo.com>
2021-12-17 19:31:14 -0600

    Day 17 p1 cleanup

--- d17.py
+++ d17.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 def make_rocket_go_now(area, steps):
-    # try a bunch of trajectories
     valid_dx = find_valid_x_trajs(area)
     print(f"Found {len(valid_dx)} valid x trajectories.")
     count = 0
@@ -18,8 +17,9 @@ def make_rocket_go_now(area, steps):
                     print(f"New high {highest_y} with trajectory ({dx},{dy})")
                     max_y = highest_y
         count += 1
+    return max_y
 
-
+# Will trajectory dx, dy hit area? Also returns highest y for dx, dy.
 def fire(_dx, _dy, area):
     dx, dy = _dx, _dy
     pos = (0,0)
@@ -33,31 +33,31 @@ def fire(_dx, _dy, area):
         dy -= 1
     return area.inside(pos[0], pos[1]), highest_y
 
-
-def fire_x(_dx, area):
-    dx = _dx
+# Will x trajectory dx be in area's x-range?
+def fire_x(dx, area):
     pos = 0
     while not area.xmax >= pos >= area.xmin and not pos > area.xmax and dx != 0:
         pos += dx
-        if dx != 0:
-            dx += -1 if dx > 0 else 1
+        dx += -1 if dx > 0 else 1
     return area.xmax >= pos >= area.xmin
 
-def fire_y(_dy, area):
-    dy = _dy
+# Will y trajectory dy be in area's y-range?
+def fire_y(dy, area):
     pos = 0
     while not area.ymax >= pos >= area.ymin and not pos < area.ymin:
         pos += dy
         dy -= 1
     return area.ymax >= pos >= area.ymin
 
+# Find all dx that will be in the target's x-range.
 def find_valid_x_trajs(area):
     valid_dx = []
-    for dx in range(1, area.xmax + 1):
+    for dx in range(1, area.xmax + 1): # dx > area.xmax will never be in range
         if fire_x(dx, area):
             valid_dx.append(dx)
     return valid_dx
 
+# Find all dy between dy_min and dy_max that will be in the target's y-range.
 def find_valid_y_trajs(area, dy_min, dy_max):
     valid_dy = []
     for dy in range(dy_min, dy_max):
@@ -82,4 +82,6 @@ class Area:
 if __name__ == "__main__":
     # area = Area(20, 30, -10, -5)
     area = Area(29, 73, -248, -194)
-    make_rocket_go_now(area, 20)
+    steps = 20
+    max_y = make_rocket_go_now(area, steps)
+    print(f"Part 1: After {steps} steps, Highest y = {max_y}.")
